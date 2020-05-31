789fb1161144e04cc7ad808ed479e95a162eb41e
Afzaal <65043912+afzaal786-comsats@users.noreply.github.com>
2020-05-31 18:03:26 +0500

    assignment last
    assignment last

---
+++ Video Tuts 2/Assets/TextScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class TextScript : MonoBehaviour
+{
+    public TextMeshPro Text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text = FindObjectOfType<TextMeshPro>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Text.text = "AFZAAL";
+    }
+}
